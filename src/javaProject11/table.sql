8ec2b4a4c574c6b1c9676864ebb518868c7a202b
cujv <kouire714@icloud.com>
2023-09-26 12:34:42 +0900

    20230926_1234 JavaProject

--- src/javaProject11/table.sql
+++ src/javaProject11/table.sql
@@ -3,18 +3,18 @@ show tables;
 create table customer(
   idx  int not null auto_increment primary key,
   id varchar(20) not null,  	/* 아이디 */
-  password  int not null,		/* 비밀번호 */
+  password int not null,		/* 비밀번호 */
   name varchar(20) not null,  	/* 성명 */
   birth  int not null,		/* 생년월일 */
   gender char(2) not null,		/* 성별 */
-  address varchar not null,	 	/* 주소 */
+  address varchar(15) not null,	 	/* 주소 */
   phone int not null 			/* 연락처 */
 );
 
 create table room2(
   idx  int not null auto_increment primary key,
   idate date not null,  	/* 체크인 날짜 */
-  odate date not null,		/* 체크아웃 날짜 */
+  odate date not null		/* 체크아웃 날짜 */
 );
 
 create table room4(
